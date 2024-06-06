f8a01ba3995927c0ee8baca5a16743794fa8f902
Yuri van der Burg <yuri.vanderburg@gmail.com>
2024-06-06 18:54:38 +0200

    Changed second_screen bash script

--- second_screen.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/bash
-
-xrandr --output HDMI-A-0 --primary
-xrandr --output HDMI-A-0 --scale 1x1
-xrandr --output HDMI-A-0 --mode 1920x1080
-xrandr --output HDMI-A-0 --dpi 192
-#feh --bg-fill ~/.config/i3/wallpaper/DSC_0596.jpg
